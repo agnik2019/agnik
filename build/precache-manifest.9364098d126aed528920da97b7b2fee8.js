95e21e1ddb38cdd607501cc2b73b092eafb9a48a
Agnik Saha <agniksaha2019@gmail.com>
2025-02-22 12:15:09 -0500

    try

---
+++ build/precache-manifest.9364098d126aed528920da97b7b2fee8.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ddd711769aac3d16eb6fcfdda9adb8d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1c023285f9f12e1beb4f",
+    "url": "/static/css/2.de8acc9e.chunk.css"
+  },
+  {
+    "revision": "495f6347099bd87857ca",
+    "url": "/static/css/main.9b5aee15.chunk.css"
+  },
+  {
+    "revision": "1c023285f9f12e1beb4f",
+    "url": "/static/js/2.4356da80.chunk.js"
+  },
+  {
+    "revision": "6c372ab34004e127660e38ef1d31eb4f",
+    "url": "/static/js/2.4356da80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "495f6347099bd87857ca",
+    "url": "/static/js/main.a4129cf1.chunk.js"
+  },
+  {
+    "revision": "2a88ccedb3f319f1da7c",
+    "url": "/static/js/runtime-main.23baeeab.js"
+  },
+  {
+    "revision": "8fb0e5fd721a02afda190255b5ee649d",
+    "url": "/static/media/about.8fb0e5fd.png"
+  },
+  {
+    "revision": "22e089fc3bfc378f7770542022087044",
+    "url": "/static/media/android.22e089fc.jpg"
+  },
+  {
+    "revision": "f5c64a5f820c4ac31423ed0ee5d95c7c",
+    "url": "/static/media/clock.f5c64a5f.svg"
+  },
+  {
+    "revision": "f620483184a3efa896127ee80cab531c",
+    "url": "/static/media/diaphragm.f6204831.svg"
+  },
+  {
+    "revision": "20c7f829a3a4c76f5496d186d30bfe51",
+    "url": "/static/media/ds1.20c7f829.jpg"
+  },
+  {
+    "revision": "9a9cab82ac0c8df725576a0dd54d9520",
+    "url": "/static/media/ds2.9a9cab82.jpg"
+  },
+  {
+    "revision": "556e69bd7e1f5a0b632ba05e82d0342a",
+    "url": "/static/media/man1.556e69bd.png"
+  },
+  {
+    "revision": "d1749735b44aafa17334e7710263aac3",
+    "url": "/static/media/money.d1749735.svg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "8c795ad57e55b06d76b8a73b27a13fd1",
+    "url": "/static/media/teamwork.8c795ad5.svg"
+  },
+  {
+    "revision": "a4ecb327bcabdd301573ebdb07ee0beb",
+    "url": "/static/media/wd.a4ecb327.jpg"
+  }
+]);
