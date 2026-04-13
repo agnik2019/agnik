c052c79f4ee845029dde8ac3293fec03c3c52214
Agnik Saha <agniksaha2019@gmail.com>
2026-04-13 16:03:58 -0400

    minor fix

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/agnik">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
